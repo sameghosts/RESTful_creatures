b2c7c51f16c808966cec53f57774c871c52a4470
sameghosts <seanfeiner.brown@gmail.com>
2021-01-11 16:32:59 -0800

    dino json added and parsed

--- routes/dinos.js
+++ routes/dinos.js
@@ -1,8 +1,12 @@
 const EXPRESS = require('express');
 const ROUTER = EXPRESS.Router();
+const fs = require('fs');
 
 //Index
 ROUTER.get('/', (req, res) => {
+  let dinos = fs.readFileSync('./dinos.json');
+  let dinoData = JSON.parse(dinos);
+  console.log(dinoData);
   res.render('dinos/index', {dinos: [] });
 });
 
