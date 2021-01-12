f211a923773b6312821999f55ea386a25435b63a
sameghosts <seanfeiner.brown@gmail.com>
2021-01-12 14:39:26 -0800

    methodoverride edit route and put

--- routes/dinos.js
+++ routes/dinos.js
@@ -14,6 +14,51 @@ ROUTER.get('/new', (req, res) =>{
   console.log('--------NEW DINO WHO DIS????');
   res.render('dinos/new');
 });
+
+//show / details - /dinos/:id
+ROUTER.get('/:id', (req, res) =>{
+  //get the index
+  let dinoIndex = req.params.id
+  //get mutuable dino data
+  let dinos = fs.readFileSync('./dinos.json');
+  let dinoData = JSON.parse(dinos);
+
+  //find dino at said index
+  let thisDino = dinoData[dinoIndex]; 
+
+  //if there is no dino at dinoData[dinoIndex]
+    //show  404 page (endgame)
+    // redirect to dino/new
+  if(!thisDino){ 
+    res.redirect('dinos/new');
+  } else {
+    //ship it
+    res.render('dinos/show', {dino: thisDino });
+  }
+});
+
+//EDIT - GET /dinos/:id/edit
+ROUTER.get('/:id/edit', (req, res) =>{
+  //send the dino into a client page which is the form for a put route
+  let dinoIndex = req.params.id
+  let dinos = fs.readFileSync('./dinos.json');
+  let dinoData = JSON.parse(dinos);
+  let thisDino = dinoData[dinoIndex]; 
+    //show  404 page (endgame)
+    // redirect to dinos
+  if(!thisDino){ 
+    res.redirect('/dinos');
+  } else {
+    //ship it
+    res.render('dinos/edit', {dino: thisDino, dinoID: dinoIndex });
+  }
+});
+
+ROUTER.put('/:id', (req, res) =>{
+  console.log(req.body);
+  res.send(`Editing dino at ${req.params.id}`);
+})
+
 //create - POST /dinos
 ROUTER.post('/', (req, res) => {
   // res.send('Postin a dino');
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //initialize express and instance as app
 const EXPRESS = require('express');
 const APP = EXPRESS();
+const METHODOVERRIDE = require('method-override');
 const path = require('path');
 
 //ejs layouts
@@ -15,6 +16,9 @@ APP.use(EXPRESS.static(path.join(__dirname, '/static')));
 
 //url encoded, body parsing middleware
 APP.use(EXPRESS.urlencoded({ extended: false}));
+
+//method override middleware
+APP.use(METHODOVERRIDE('_method'));
 //global routes
 APP.get('/', (req, res) => {
   res.render('home');
